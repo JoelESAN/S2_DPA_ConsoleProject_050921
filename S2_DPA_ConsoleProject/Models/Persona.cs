2ee668829a42fa411daed26c0cbc42a6a1307360
JoelESAN <82479782+JoelESAN@users.noreply.github.com>
2021-09-05 13:05:42 -0500

    Agregar archivos de proyecto.

---
+++ S2_DPA_ConsoleProject/Models/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2_DPA_ConsoleProject.Models
+{
+    public class Persona
+    {
+        public string dni { get; set; }
+        public String nombreCompleto { get; set; }
+
+        public int telefono { get; set; }
+
+        public Persona ()
+        {
+
+
+        }
+
+        public Persona(string dni, string nombreCompleto, int telefono)
+        {
+            
+        }
+
+    }
+}
